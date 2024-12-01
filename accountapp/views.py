c9db30cff642ea2b868054e6111ce7f007f37996
leedaae <lee@leedaaeui-MacBookPro.local>
2024-12-01 22:09:52 +0900

    accountapp add

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DetailView, UpdateView
 
+from accountapp.forms import AccountUpdateForm
 from accountapp.models import HelloWorld
 
 # Create your views here.
@@ -13,10 +18,38 @@ def hello_world(request):
         new.text = temp
         new.save() # db에 저장됨
 
-        #HelloWorld.objects.all() # 모두 가져옴
+        hello_world_list = HelloWorld.objects.all() # 모두 가져옴
 
         #return render(request, 'accountapp/hello_world.html', context={'text':temp})
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output':new})#객체 내보냄
+        #return render(request, 'accountapp/hello_world.html', context={'hello_world_output':new})#객체 내보냄
+        #return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET method'})
+        #return render(request, 'accountapp/hello_world.html', context={'text':'GET method'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_list':hello_world_list})
+
+# 클래스 base view
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world') # 로그인 성공하면 재연결
+    # 함수형 뷰 reverse() 클래스형 뷰 reverse_lazy()
+    template_name = 'accountapp/create.html'
+
+class AccountDetailView(DetailView):
+    model = User
+    context_object_name = 'target_user'
+    template_name = 'accountapp/detail.html'
+
+class AccountUpdateView(UpdateView):
+    model = User
+    form_class = AccountUpdateForm
+    success_url = reverse_lazy('accountapp:hello_world') 
+    template_name = 'accountapp/update.html'
+
+class AccountDeleteView(DetailView):
+    model = User
+    success_url = reverse_lazy('accountapp:login')
+    template_name = 'accountapp/delete.html'
 
--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+from django.conf.global_settings import LOGIN_REDIRECT_URL
+from django.urls import reverse_lazy
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accountapp',
 ]
 
@@ -129,3 +133,6 @@ STARICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
