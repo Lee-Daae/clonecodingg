492f105c971284d873027c4dc4fd328c4e5c3a34
Lee-Daae <da.lee@haezoom.com>
2024-12-16 11:12:03 +0900

    transaction

--- likeapp/views.py
+++ likeapp/views.py
@@ -1,6 +1,8 @@
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db import transaction
+from django.forms import ValidationError
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
@@ -9,6 +11,19 @@ from django.views.generic import RedirectView
 
 from articleapp.models import Article
 from likeapp.models import Like
+
+@transaction.atomic
+def db_transaction(user, article):
+
+    article.like += 1
+    article.save()
+
+    if Like.objects.filter(user=user, article=article).exists():
+        raise ValidationError('Like already exists')
+    else:
+        Like(user=user, article=article).save()
+    
+
 
 @method_decorator(login_required, name='get')
 class LikeArticleView(RedirectView):
@@ -19,17 +34,13 @@ class LikeArticleView(RedirectView):
 
         user = self.request.user
         article = get_object_or_404(Article, pk=kwargs['pk'])
-
-        if Like.objects.filter(user=user, article=article).exists():
+        try:
+            db_transaction(user, article)
+            messages.add_message(self.request, messages.SUCCESS, '좋아요 반영')
+        except ValidationError:
             messages.add_message(self.request, messages.ERROR, '좋아요 한번가능')
             return HttpResponseRedirect(reverse('articleapp:detail', kwargs={'pk': kwargs['pk']}))
-        else:
-            Like(user=user, article=article).save()
-        
-        article.like += 1
-        article.save()
-
-        messages.add_message(self.request, messages.SUCCESS, '좋아요 반영')
+            
 
         return super(LikeArticleView, self).get(self.request, *args, **kwargs)
     
